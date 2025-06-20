125cb87bb02b26cb36593f8aa6b9d530969625c6
Richard Ryan Marroquin <marroquinryan@yahoo.com>
2025-06-20 07:44:02 -0700

    Day 3 Solutions, used different methods like print statements, try/catch, and IDE debugger to debug code bugs.

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# The for loop iterates from 1 to 20 checking if the variable i is equal to 20. If it is equal print out something.
 # 2. When is the function meant to print "You got it"?
+# When i the iterator hits 20.
 # 3. What are your assumptions about the value of i?
+# i is declared and used in the for loop.
--- Day 13/Use a Debugger/task.py
+++ Day 13/Use a Debugger/task.py
@@ -9,8 +9,10 @@ def mutate(a_list):
         new_item = item * 2
         new_item += random.randint(1, 3)
         new_item = maths.add(new_item, item)
-    b_list.append(new_item)
+        b_list.append(new_item) #the append was done after the for loop.
     print(b_list)
 
 
 mutate([1, 2, 3, 5, 8, 13])
+
+#Assuming the issue is b_list is not the same length as a_list
